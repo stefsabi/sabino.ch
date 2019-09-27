c4a8fe6ab333fe339c1c20d410c516683921de8d
Stefano Sabino <stefano.sabino@gmail.com>
2019-09-27 10:33:41 +0200

    Basis
    basis

---
+++ index.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Familie Sabino">
+    <meta name="author" content="Stefano Sabino">
+
+    <title>Familie Sabino</title>
+
+    <!-- Bootstrap core CSS -->
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+
+    <!-- Custom styles for this template -->
+    <link href="style.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href='https://fonts.googleapis.com/css?family=Great+Vibes' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900' rel='stylesheet' type='text/css'>
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+<div class="site-wrapper video-background">
+    <!--
+    Video from YouTube
+    Have Questions? How To:
+    https://github.com/pupunzi/jquery.mb.YTPlayer/wiki
+    -->
+    <a id="bgndVideo" class="player" data-property="{videoURL:'https://youtu.be/-R6XJ66kJJQ', containment:'body', autoPlay:true, mute:true, startAt:1, stopAt: 53, opacity:1, }"></a>
+
+    <div class="overlay"></div>
+
+    <div class="site-wrapper-inner">
+
+            <div class="masthead clearfix">
+                <div class="inner">
+                    <h3 class="masthead-brand"><i class="fa fa-phone fa-fw"><a href="tel:0041794602323"></i> (+41) 79-460 23 23<br><small>click to call <i class="fa fa-level-up"></i></small></a></h3>
+                    <nav>
+                        <ul class="nav masthead-nav">
+                            <li><a href="https://twitter.com/stefsabi"><i class="fa fa-twitter fa-fw"></i></a></li>
+                            <li><a href="https://www.facebook.com/stefano.sabino"><i class="fa fa-facebook fa-fw"></i></a></li>
+                            <li><a href="https://plus.google.com/+StefanoSabino/"><i class="fa fa-google-plus fa-fw"></i></a></li>
+                            <li><a href="mailto:info@daten-server.com"><i class="fa fa-envelope fa-fw"></i></a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+
+            <div class="cover-container">
+            <div class="inner cover">  <h1 class="cover-heading"><a href="index2.html"><img src="img/sabino_name_schrift_weiss.png"  height="100" alt="sabino"/></a></h1>
+              <br><br><p><img src="img/sabino_print-logo_krone.png"  height="20" alt="sabino"/></p><p>Nobili - Feudatari - Patrizi - Conti</p>
+              <div><img src="img/sabino_print-logo1.png"  height="250" alt="sabino"/>
+              <img src="img/sabino_print-logo3.png"  height="250" alt="sabino"/><p><small>Blasone della famiglia Sabino o Sabini di Altamura Fonte bibliografica "Dizionario storico blasonico delle famigle noboli e notabili italiane estinte e fiorenti" vol.II compilato dal Comm. G.B. di Crollalanza edito presso la direzione del giornale, Pisa 1888.</small></p><p><small>Antica ed assai nobile famiglia feudataria d'Abruzzo, con prima residenza in Chieti, conosciuta come Sabini o Sabino, propagatasi, nel corso dei secoli, in diverse regioni d’Italia e del Mondo. La leggenda narra che la Famiglia discenda da Raino, Conte della Sabina nel secolo IX, appartenente ad una stirpe ramo della celebre casata dei Crescenzie, che ha primeggiato nella storia di Roma dalla metà del secolo X alla metà del secolo XI. Ciò di cui invece storicamente siamo certi e che tal famiglia passò a risiedere in L’Aquila sul calar del secolo decimo sesto ed ivi venne ascritta al Patriziato. Estintasi nel ramo primogenito nel secolo decimo settimo, la famiglia Sabino sopravvisse e sopravvive tutt’ora nei rami collaterali, tra cui quello che in Lanciano diede personaggi distintisi come organai e musicisti. E’ fiorita in Altamura un ’altra omonima famiglia, la cui parentela con la precedente non è stata storicamente accertata, alla quale appartenne quel Bernardo che, nel 1497, per gli eminenti servigi resi da lui e dai suoi maggiori alla Regia Casa d’Aragona, venne dal re Federico riconosciuto nei privilegi e nel titolo di conte palatino, già concessi dall’imperatore Enrico (VI ad un Angelo, suo antenato. Similmente in (Veneto ove iS. furono nobili in Venezia. Da una analisi cromatica e simbolica dello stemma, possiamo, infine, ricavare alcune importanti informazioni sulle virtù associate a tal cognome. L’azzurra essendo il colore del cielo, simboleggia tutte le virtù più elevate e, tra quelle spirituali, devozione, fedeltà, castità, giustizia, santità. Per le qualità mondane simboleggia bellezza, nobiltà, fortezza, fermezza incorruttibili, vigilanza, vittoria, perseveranza, ricchezza, amore per la patria, buon augurio, fama gloriosa. Il leone d’oro in campo azzurro rappresenta il valore di un capitano che con la prudenza giunse ai più alti onori. La torre è contrassegno d’antica e cospicua nobiltà, perché' nessuno fino dai tempi remoti, poteva fabbricar torri se non era d’illustre e potente famiglia. I cognomi così antichi, senza dubbio alcuno, rendono decoro non solo alle famiglie ma anche alle città che li hanno accolti; pare siano, infatti, molto riguardevoli i soggetti viventi di questa famiglia che, con tanto decoro, sostentano
+questo nobile cognome, che ha avuto vari soggetti illustri.
+</small></p></div>
+
+                <p class="lead">&nbsp;</p>
+
+            </div>
+
+            <div class="mastfoot">
+                <div class="inner">
+                    <p>Copyright &copy; by Stefano Sabino <?php echo date("Y"); ?>         </p>
+                    <p><!-- Yandex.Metrika informer -->
+<a href="https://metrika.yandex.com/stat/?id=28019706&amp;from=informer"
+target="_blank" rel="nofollow"><img src="https://informer.yandex.ru/informer/28019706/1_0_FFFFFFFF_EFEFEFFF_0_pageviews"
+style="width:80px; height:15px; border:0;" alt="Yandex.Metrica" title="Yandex.Metrica: data for today (page views)" class="ym-advanced-informer" data-cid="28019706" data-lang="en" /></a>
+<!-- /Yandex.Metrika informer -->
+
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(28019706, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/28019706" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter --></p>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+</div>
+
+<!-- jQuery -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+
+<!-- Bootstrap core JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+
+<!-- Plugins and Custom JavaScript -->
+<script src="js/device.min.js"></script>
+<script src="js/jquery.mb.YTPlayer.js"></script>
+<script src="js/custom.js"></script>
+<script type='text/javascript' data-cfasync='false'>window.purechatApi = { l: [], t: [], on: function () { this.l.push(arguments); } }; (function () { var done = false; var script = document.createElement('script'); script.async = true; script.type = 'text/javascript'; script.src = 'https://app.purechat.com/VisitorWidget/WidgetScript'; document.getElementsByTagName('HEAD').item(0).appendChild(script); script.onreadystatechange = script.onload = function (e) { if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) { var w = new PCWidget({c: '6781a822-36f5-4379-95f8-3645c14eaa02', f: true }); done = true; } }; })();</script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-1569918-2"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-1569918-2');
+</script>
+
+</body>
+</html>
